e59560e7c704bef94fde5e19e6eaeddad5ee7804
Alex Wilson <alex@cooperi.net>
2012-05-28 23:01:35 +1000

    Adding scan reports

--- src/scanner.erl
+++ src/scanner.erl
@@ -6,12 +6,14 @@ start_link() ->
 
 scan_existing() ->
     ExistingPorts = lists:map(fun({Port, _Info}) -> Port end, db:get_list()),
+    error_logger:info_report([{scan, existing}, {count, length(ExistingPorts)}]),
     lists:foreach(fun(Port) -> checker_pool:add_job(Port) end, ExistingPorts).
 
 scan_new() ->
     ExistingPorts = lists:map(fun({Port, _Info}) -> Port end, db:get_list()),
     AllPorts = netstat:candidates(),
     NotExistingPorts = AllPorts -- ExistingPorts,
+    error_logger:info_report([{scan, new}, {count, length(NotExistingPorts)}]),
     lists:foreach(fun(Port) -> checker_pool:add_job(Port) end, NotExistingPorts).
 
 wait(Secs) ->
@@ -23,6 +25,7 @@ wait(Secs) ->
 loop() ->
     Flong = dist_var:new(30, 10),
     Fshort = dist_var:new(15, 5),
+    wait(5),
     scan_new(), wait(Flong()),
     scan_existing(), wait(Flong()),
     scan_existing(), wait(Flong()),
